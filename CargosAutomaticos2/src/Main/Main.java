3f9a070d7b3d635bedcc9fd980ed843e13396de5
bradguillen15 <bradguillen15@gmail.com>
2016-03-22 15:15:34 -0600

    Metodos a la BD

--- CargosAutomaticos2/src/Main/Main.java
+++ CargosAutomaticos2/src/Main/Main.java
@@ -7,8 +7,7 @@ public class Main {
     public static void main(String[] args) {
         DataBase theDatabase = new DataBase();
         theDatabase.OpenConecctionToDatabase();
-        //System.out.println(theDatabase.GetInformationFromDatabase("SELECT * FROM cliente"));
-        System.out.println(theDatabase.ValidateThePayment("1111111111111111", "111"));
+        
         theDatabase.CloseConecctionToDatabase();
     }
 }
